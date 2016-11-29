f39227cc55d7fed8b79ecd64202132367fda4671
WalissonRodrigo <walissonrodrigo@outlook.com>
2016-11-29 15:08:48 -0200

    configuração do settings

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -169,3 +169,8 @@ AUTH_USER_MODEL = 'accounts.User'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
