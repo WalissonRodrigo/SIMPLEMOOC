b40a9006a195f324402bdac6e14bb1befe118e59
WalissonRodrigoSaga <walissonrodrigo@outlook.com>
2016-11-30 11:15:37 -0200

    alteração no settings

--- simplemooc/wsgi.py
+++ simplemooc/wsgi.py
@@ -15,7 +15,7 @@ from dj_static import Cling
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simplemooc.settings")
-
 application = get_wsgi_application()
+
 application = Cling(get_wsgi_application())
 application = DjangoWhiteNoise(application)
