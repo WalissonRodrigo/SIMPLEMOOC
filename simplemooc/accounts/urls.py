460d6853674d6f5c6b625dfe0efe886e0db9198b
WalissonRodrigo <walissonrodrigo@outlook.com>
2016-12-01 07:38:54 -0200

    correção arquivos do projeto

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     url(r'^entrar/$', login, {'template_name': 'accounts/login.html'}, name='login'),
     url(r'^sair/$', logout, {'next_page': 'core:home'}, name='logout'),
     url(r'^nova-senha/$', password_reset, name='password_reset'),
-url(r'^cadastre-se/$', register, name='register'),
+    url(r'^confirmar-nova-senha/(?P<key>\w+)/$', password_reset_confirm, name='password_reset_confirm'),
+	url(r'^cadastre-se/$', register, name='register'),
     url(r'^editar/$', edit, name='edit'),
     url(r'^editar-senha/$', edit_password, name='edit_password'),
 ]
