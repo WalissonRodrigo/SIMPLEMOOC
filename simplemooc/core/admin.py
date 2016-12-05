ed43a325feed068a60b9604d0da82e5bd0837027
Walisson Rodrigo <walissonrodrigo@outlook.com>
2016-12-05 15:53:42 -0200

    alteração configurações do django

--- simplemooc/core/admin.py
+++ simplemooc/core/admin.py
@@ -1,10 +1,2 @@
 from django.contrib import admin
-from .models import Empresa
-# Register your models here.
-class EmpresaAdmin(admin.ModelAdmin):
-    list_display = ('id_emp', 'cnpj', 'razao', 'fantasia', 'endereco', 'cidade', 'uf', 'cep', 'ativo', 'data_cadastro')
-    list_display_links = ('id_emp', 'cnpj', 'razao', 'fantasia', 'endereco', 'cidade', 'uf', 'cep', 'ativo', 'data_cadastro')
-    list_filter = ('id_emp', 'cnpj', 'razao', 'fantasia', 'cidade', 'uf', 'ativo')
-    search_fields = ('id_emp', 'cnpj', 'razao', 'fantasia', 'cidade', 'uf', 'ativo')
 
-admin.site.register(Empresa, EmpresaAdmin)
