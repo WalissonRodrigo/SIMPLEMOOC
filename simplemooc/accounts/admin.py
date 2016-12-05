cf88bfd3e1b856660ae2709c6f6e2def3bbd8c26
Walisson Rodrigo <walissonrodrigo@outlook.com>
2016-12-05 16:03:22 -0200

    Inclusão do Usuario no Admin

--- simplemooc/accounts/admin.py
+++ simplemooc/accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User
+
+class UserAdmin(admin.ModelAdmin):
+	list_display = ['username', 'email', 'name', 'is_active', 'is_staff', 'date_joined']
+	search_fields = ['username', 'email', 'name', 'is_active', 'is_staff', 'date_joined']
+	prepopulated_fields = {'username':('name', 'email')}
+
+
+admin.site.register(User, UserAdmin)
